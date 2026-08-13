2704a32189c17dfce482198024ee13603e8b1624
yasir23 <yasirkhalily44@gmail.com>
2026-08-13 15:59:13 +0500

    add scroll-margin for anchored sections (bump CSS hash to bypass CF 404 cache)

--- docs/_next/static/chunks/app/blog/find-hospital-mrf-file/page-d05511bce5181f17.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[183,192,978,389,669,134,201,745,896,713,327,341,955,979,577,614,469],{8625:function(n,e,u){Promise.resolve().then(u.t.bind(u,231,23))}},function(n){n.O(0,[231,971,23,744],function(){return n(n.s=8625)}),_N_E=n.O()}]);
